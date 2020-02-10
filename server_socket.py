bb92fa2d36caf29d8f80b001617f820e73cecaaa
Alejandro Jaime G <alejandro.esteban@outlook.com>
2020-02-09 21:38:14 -0300

    Init commit

---
+++ server_socket.py
@@ -0,0 +1,174 @@
+import os
+from datetime import datetime
+import socket 
+from threading import Thread
+import time
+import argparse
+import multiprocessing as mp
+import thread
+import subprocess
+import codecs
+
+       
+        
+class SocketServer():
+    '''
+    This class creates a socket server and listens to messages on a specific ip and port.
+    '''
+    clients = []
+    
+    def __init__(self, ip='127.0.0.1', port='8080', folder_path='data', timer=60):
+        '''
+        Parameters
+        
+        ip: int
+            Ip to receive messages with web socket.
+        port: int
+            Port to receive messages with web socket.
+        folder_path: string
+            Path to save messages received with web socket.
+        timer: int
+            Timer to wait to last messages or close connection with client.
+        '''
+        self.ip = ip
+        self.port = port
+        self.folder_path = folder_path
+        self.timer = timer
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        self.socket.bind((self.ip, self.port)) 
+        self.socket.listen(5) 
+        
+        def run(self):
+            print ("Server started")
+        try:
+            self.accept_clients()
+        except Exception as ex:
+            print (ex)
+        finally:
+            print ("Server closed")
+            for client in self.clients:
+                client.close()
+            self.socket.close()
+            
+    def accept_clients(self):
+        '''
+        Accet client to connect this and create thread to process client.
+        '''
+        while 1:
+            print("Multithreaded Python server : Waiting for connections in IP:", self.ip, " PORT:",self.port)
+            (clientsocket, address) = self.socket.accept()
+            clientsocket.setblocking(0)
+            #Adding client to clients list
+            self.clients.append(clientsocket)
+            #Client Connected
+            self.onopen(clientsocket, address)
+            #Receiving data from client
+            thread.start_new_thread(self.recieve, (clientsocket,address))
+            
+    def recieve(self, client, address):
+        '''
+        Receive messages from client trough onmessage and onclose
+        '''
+        max_buffer_size = 256
+        #Determine the time lapsed from first connection with same client, if time minour that timer return
+        tic = time.time()
+        while 1:
+            data_brute, port  = client.recvfrom(max_buffer_size)
+            #data_brute = client.recv(max_buffer_size)
+            toc = time.time() - tic
+            if not data_brute or toc > self.timer:
+                break   
+            #Message Received
+            self.onmessage(client, data_brute, address)
+        #Removing client from clients list
+        self.clients.remove(client)
+        #Client Disconnected
+        self.onclose(client,address, data_brute)
+        #Closing connection with client
+        client.close()
+        #Closing thread
+        thread.exit()
+        
+    def onopen(self, client, address):
+        '''
+        Trigger when new client connected.
+        '''
+        print ("[+] New server socket thread started for " + str(address))
+
+
+    def onmessage(self, client, message, address):
+        '''
+        Trigger when response to client with a message.
+        '''
+        self.save_data(address[0], message)
+        MESSAGE = b'\nMessage received by server\n'
+        client.send(MESSAGE)  # echo
+
+    def onclose(self, client, address, data):
+        '''
+        Trigger to print close connection with client.
+        '''
+        print("Close connection",address, data)
+        pass    
+    
+    def save_data(self, addr, data):
+        '''
+        Save in txt and call php function when receive data from client.
+        
+        Parameters
+        ----------
+        addr: tuple int (ip,port)
+            Address from connected client
+            
+        data: bytes
+            Data send from client to server.
+        '''
+        #Save data in txt and log adding lines to document.
+        file_path = os.path.join(self.folder_path,'data.txt')
+        log_path = os.path.join('log','log.txt')
+        
+        if not os.path.isdir(self.folder_path):
+            os.makedirs(self.folder_path)
+            
+        if not os.path.isdir('log'):
+            os.makedirs('log')
+        
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            
+        with open(file_path, 'w') as file:            
+            file.write(dt_string +','+data_decode+'\n') 
+            
+        with open(log_path, 'a') as file:
+            file.write(dt_string +','+data_decode+'\n')
+             
+        subprocess.call(["php", "/var/www/rabbit/admin/html/thingsend.php"])
+        
+                       
+def main(ip='127.0.0.1',port=18000, folder_path='data'): 
+    '''
+    Funtion principal to create object SocketServer from websocket.
+    '''              
+    # Multithreaded Python server : TCP Server Socket Program Stub
+    SocketServer(ip, port, folder_path)    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('parameters',
+                        type=str, nargs=3,
+                        help='host, port and folder')
+    args = parser.parse_args()
+
+    ip = args.parameters[0]
+    port = int(args.parameters[1])
+    folder_path = os.path.abspath(args.parameters[2])
+    
+    server_process = mp.Process(name="{server_socket}",
+                                 args=(ip, port, folder_path),
+                                 target=main)
+    server_process.start()
+
+
+
+
